562fba4bfdc75c1305e25f646ba0e67d0cdaaded
kojiyamauchi <all.the.small.things@me.com>
2020-10-15 20:56:28 +0900

    update webpack.***.babel.js

--- webpack/webpack.pro.babel.js
+++ webpack/webpack.pro.babel.js
@@ -5,7 +5,6 @@
 import webpack from 'webpack'
 import { merge } from 'webpack-merge'
 import TerserPlugin from 'terser-webpack-plugin'
-// import LicenseInfoWebpackPlugin from 'license-info-webpack-plugin'
 import webpackBase from './webpack.base.babel'
 
 // Base Setting by webpack.gulp.base.babel.js.
@@ -13,14 +12,6 @@ export default merge(webpackBase, {
   mode: 'production',
   // Setting for Plugins.
   plugins: [
-    /*
-    When Minify of Production's JS File, Keep License Comment Out.
-    When Use This Plugins with webpack5, Warning on Terminal.
-    So Temporary Disabled.　
-    new LicenseInfoWebpackPlugin({
-      glob: '{LICENSE,license,License}*'
-    }),
-    +/
     /* Even when it is already sufficiently compressed,
     the code can be analyzed in detail and the parts
     that are likely to be commonly compressed are compressed more positively */
